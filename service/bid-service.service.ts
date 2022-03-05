ec2c9903bc9bb9a1f2119d314804c32199459aa4
vanchimca <vanchi.nat@gmail.com>
2022-03-05 20:42:32 +0530

    updated files and properties for cloud

--- service/bid-service.service.ts
+++ service/bid-service.service.ts
@@ -11,7 +11,7 @@ export class BidServiceService {
   private bidsUrl: string;
 
   constructor(private http: HttpClient) {
-    this.bidsUrl = 'http://localhost:8091/e-auction/api/v1/buyer/bids/';
+    this.bidsUrl = 'http://ec2-3-88-0-13.compute-1.amazonaws.com:8091/e-auction/api/v1/buyer/bids/';
   }
 
   public findAll(productId: string): Observable<BidDetails[]> {
@@ -19,11 +19,11 @@ export class BidServiceService {
   }
 
   public save(bid: BidDetails) {
-    this.bidsUrl = 'http://localhost:8091/e-auction/api/v1/buyer/place-bid';
+    this.bidsUrl = 'http://ec2-3-88-0-13.compute-1.amazonaws.com:8091/e-auction/api/v1/buyer/place-bid';
     return this.http.post(this.bidsUrl, bid, { responseType: 'text' });
   }
   public update(bidId: String, mail: string, bidAmount: number, productId:string) {
-    this.bidsUrl = 'http://localhost:8091/e-auction/api/v1/buyer/updateBid/';
+    this.bidsUrl = 'http://ec2-3-88-0-13.compute-1.amazonaws.com:8091/e-auction/api/v1/buyer/updateBid/';
     return this.http.get(this.bidsUrl+bidId+"/"+mail+"/"+bidAmount+"/"+productId, { responseType: 'text' });
   }
 }
